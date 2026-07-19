1ad04e6ecc965f2f4fa6f2d785d9c283f8717934
Get <getdna99@gmail.com>
2026-07-19 17:24:46 +0700

    fix: WinUI build error

--- src/DesktopFlyouts.Shared/XamlIslandHostWindow.cs
+++ src/DesktopFlyouts.Shared/XamlIslandHostWindow.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Graphics;
 using Windows.Win32;
