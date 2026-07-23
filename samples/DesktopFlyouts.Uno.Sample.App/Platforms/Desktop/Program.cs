72f35f07d09e83e8f7fe97e9d0940dd3c8f9479f
Get <getdna99@gmail.com>
2026-07-23 21:15:06 +0700

    delete non Linux stuff

--- samples/DesktopFlyouts.Uno.Sample.App/Platforms/Desktop/Program.cs
+++ samples/DesktopFlyouts.Uno.Sample.App/Platforms/Desktop/Program.cs
@@ -10,9 +10,6 @@ internal class Program
         var host = UnoPlatformHostBuilder.Create()
             .App(() => new App())
             .UseX11()
-            .UseLinuxFrameBuffer()
-            .UseMacOS()
-            .UseWin32()
             .Build();
 
         host.Run();
